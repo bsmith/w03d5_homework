809f26a11b11498e0275f133c47aebca4bdbc94b
Benjamin Smith <bsmith@cabbage.org.uk>
2022-11-06 15:43:17 +0000

    Change web app to use ids not indices.

--- controllers/books.py
+++ controllers/books.py
@@ -1,22 +1,22 @@
 from flask import Blueprint, request, render_template, url_for, redirect
 
 from models.book import Book
-from models.library import library, add_new_book, delete_book
+from models.library import library
 
 # Blueprints! https://flask.palletsprojects.com/en/2.2.x/blueprints/
 books = Blueprint("books", __name__)
 
 @books.route('/books')
 def books_list():
-    return render_template("books_list.html.j2", title="All Books", books_list=library)
+    return render_template("books_list.html.j2", title="All Books", books_list=library.get_all_books())
 
-@books.route('/books/<int:index>')
-def books_single(index: int):
-    if index >= len(library):
+@books.route('/books/<int:accession_id>')
+def books_single(accession_id: int):
+    book = library.find_book_by_accession_id(accession_id)
+    if book is None:
         # 404 error instead?
-        raise ValueError("books_single received index out of range of library")
-    book = library[index]
-    return render_template("books_single.html.j2", title="View Book", book=book, index=index)
+        raise ValueError("accession_id not found")
+    return render_template("books_single.html.j2", title="View Book", book=book)
 
 @books.route('/books', methods=['POST'])
 def books_create():
@@ -29,21 +29,27 @@ def books_create():
 
     # build object
     new_book = Book(**book_data)
-    library.append(new_book)
-    new_index = len(library) - 1
-    return redirect(url_for('books.books_single', index=new_index))
+    library.add_new_book(new_book)
+    new_accession_id = new_book.accession_id
+    return redirect(url_for('books.books_single', accession_id = new_accession_id))
 
-@books.route('/books/<int:index>/delete', methods=['POST'])
-def books_delete(index: int):
-    delete_book(library[index])
+@books.route('/books/<int:accession_id>/delete', methods=['POST'])
+def books_delete(accession_id: int):
+    book = library.find_book_by_accession_id(accession_id)
+    if book is None:
+        raise ValueError("accession_id not found")
+    library.delete_book(book)
     return redirect(url_for('books.books_list'))
 
-@books.route('/books/<int:index>/checkoutin', methods=['POST'])
-def books_checkoutin(index: int):
+@books.route('/books/<int:accession_id>/checkoutin', methods=['POST'])
+def books_checkoutin(accession_id: int):
+    book = library.find_book_by_accession_id(accession_id)
+    if book is None:
+        raise ValueError("accession_id not found")
     if request.form['direction'] == "in":
-        library[index].check_in()
+        book.check_in()
     elif request.form['direction'] == "out":
-        library[index].check_out()
+        book.check_out()
     else:
         raise ValueError("unknown direction")
     print(request.form['next_page'])
@@ -51,6 +57,6 @@ def books_checkoutin(index: int):
     #         request.form['next_page'] in ['books.books_list', 'books.books_single']):
     #     return redirect(url_for(request.form['next_page'], index=index))
     if 'next_page' in request.form and request.form['next_page'] == 'books.books_single':
-        return redirect(url_for('books.books_single', index=index))
+        return redirect(url_for('books.books_single', accession_id=accession_id))
     else:
         return redirect(url_for('books.books_list'))
